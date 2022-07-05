9450c35ae371d329ecd883f8776cedd56bb6c14b
Sakhawat Hossain <biplobbms@gmail.com>
2022-07-05 21:46:54 +0600

    Header Done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header/Header';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 function App() {
